aa164b875cce7cfe2db333b6bf56f6a7830ae56e
Md. Emrul Kayes <memrul21.ses@bu.ac.bd>
2024-06-08 23:33:43 +0600

    Add files via upload

---
+++ blood_bank_management/index.php
@@ -0,0 +1,98 @@
+<html>
+	<head>
+		<title>Home</title>
+		<link rel="stylesheet" href="style1.css">
+	</head>
+<body>
+<header class="heardersection">
+        <nav>
+            <div class="logo">
+                <a href="">BBM</a>
+            </div>
+            <ul class="nav-links">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="">Services</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+			
+    </header>
+
+	<div class="cover">
+	<div class="banner">
+	 <h1>Welcome to blood bank<br> management</h1>
+	</div>
+<div class="bttn">
+	 <a href="registraton.php">Registration here</a>
+	  
+	</div>
+	</div>
+		<div class="about_us">
+			<div class="aboutus">
+			 <h2>About us</h2>
+			 <p>Welcome to Blood Bank Management, a dedicated platform for managing blood donations and ensuring that those in need receive timely assistance. 
+			 Our mission is to save lives by bridging the gap between blood donors and recipients through efficient management and coordination.</p>
+			 
+           
+        </div>
+           
+        </div>
+	<div class="class_containar">
+	 <div class="aboutt">
+		<div class="card">
+
+        <img src="card.jpg">
+        
+            <h2>Md. Emrul Kayes</h2>
+            <p>President of blood bank management</p>
+           
+        </div>
+		<div class="card">
+
+        <img src="card.jpg">
+        
+            <h2>Md. Emrul Kayes</h2>
+            <p>President of blood bank management</p>
+           
+        </div>
+		<div class="card">
+
+        <img src="card.jpg">
+        
+            <h2>Md. Emrul Kayes</h2>
+            <p>President of blood bank management</p>
+           
+        </div>
+    
+	
+    </div>
+	</div>
+	
+		
+		 <div class="contact_container">
+            <h1>Contact Us</h1>
+            <p>If you have any questions or need further information, please feel free to contact us. We're here to help!</p>
+            
+            <div class="contact-info">
+                <div class="info-item">
+                    <h3>Address</h3>
+                    <p>Behind the Sonargaon Textile, Rupatali, Barishal</p>
+                </div>
+                <div class="info-item">
+                    <h3>Email</h3>
+                    <p>memrul21.ses@bu.ac.bd</p>
+                </div>
+                <div class="info-item">
+                    <h3>Phone</h3>
+                    <p>01302840538</p>
+                </div>
+            </div>
+		 </div>	
+		 
+		 <div class="footer" style="background-color:red;width:100%; height:50px;paddding:; margin-top:-20px; line-height:50px;">
+		 
+            <p align="center">&copy; 2024 Md. Emrul Kayes. All rights reserved.</p>
+        </div>
+</body>
+</html>
